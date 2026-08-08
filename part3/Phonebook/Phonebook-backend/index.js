2d30f292b7a4abae9e5ff1011e06103b55eb1596
verdevia <146423357+verdevia@users.noreply.github.com>
2026-08-08 15:33:20 +0300

    Hosting changes

--- part3/Phonebook/Phonebook-backend/index.js
+++ part3/Phonebook/Phonebook-backend/index.js
@@ -82,6 +82,5 @@ app.get('/info', (request, response) => {
 })
 
 const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-    console.log(`Server is running on port: ${PORT}`)
-})
+
+module.exports = app
